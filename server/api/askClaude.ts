bda643b18b219da12c54d8b9955863bc1e577657
fxpoet <im@fxpoet.com>
2024-07-09 21:25:29 +0900

    debug 1

--- server/api/askClaude.ts
+++ server/api/askClaude.ts
@@ -4,13 +4,16 @@ export default defineEventHandler(async (event) => {
 
     const query = getQuery(event)
     const question = query.question || '';
+    console.log('askClaude', question)
 
     if (!question)
         throw createError({ statusCode: 400, message: '질문이 필요합니다.' });
-    
-    try {        
-        return await askClaude(question)        
-    } catch (error) {        
+
+    try {
+        let r = await askClaude(question)
+        console.log('r:', r)
+        return r;
+    } catch (error) {
         throw createError({ statusCode: 500, message: 'Claude API 호출 중 오류가 발생했습니다.' })
     }
 })
