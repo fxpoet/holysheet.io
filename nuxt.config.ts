8244b769456a59bfe5f53c557bcffd2e3890f3a5
fxpoet <im@fxpoet.com>
2024-07-14 21:20:40 +0900

    pinia 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
 
   compatibilityDate: "2024-07-04",
   extends: ['@nuxt/ui-pro'],
-  modules: ["@nuxt/ui"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+  ],
   runtimeConfig: {
     CLAUDE_KEY: process.env.CLAUDE_KEY
   },
